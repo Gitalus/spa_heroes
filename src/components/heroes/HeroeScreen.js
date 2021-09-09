71294e6780afb51d4fbc2f24730236baef7ba686
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-09 13:00:21 -0300

    get heroes by id in heroeScreen and redirect if not exist

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router'
+import { getHeroeById } from '../../selectors/getHeroById';
 
 export const HeroeScreen = () => {
+
+    const { heroeId } = useParams();
+
+    const hero = getHeroeById(heroeId);
+
+    if (!hero) {
+        return <Redirect to="/" />
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters
+    } = hero;
+
     return (
         <div>
-            <h1>Heroe Screen</h1>
+            <h1>Heroe</h1>
+            <hr />
+            <h1>{superhero}</h1>
         </div>
     )
 }
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -13,7 +13,7 @@ export const DashboardRoutes = () => {
             <div className="container mt-2">
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen} />
-                    <Route exact path="/heroe/:heroeId" component={HeroeScreen} />
+                    <Route exact path="/hero/:heroeId" component={HeroeScreen} />
                     <Route exact path="/dc" component={DcScreen} />
 
                     <Redirect to="/marvel" />
