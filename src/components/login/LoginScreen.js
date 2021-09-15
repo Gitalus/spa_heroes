cf31e78ca037d71d3fea4ede3a36370056dbf590
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-15 20:13:48 -0300

    use localstorage for saving changes in user state

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -10,13 +10,14 @@ export const LoginScreen = ({ history }) => {
         // history.push('/');
         // Reemplaza la última ruta, por lo tanto no queda disponible para volver a login
         // ya que la última corresponde a /login
-        // history.replace('/');
         dispatch({
             type: types.login,
             payload: {
                 name: 'Italo'
             }
         })
+
+        history.replace('/');
     }
 
 
