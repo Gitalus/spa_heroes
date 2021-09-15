6b2e465ee9f2639a030ab8e7c4396825a1555811
Italo Guajardo <italog.penaloza@gmail.com>
2021-09-15 18:12:39 -0300

    create auth reducer

---
+++ src/auth/authReducer.js
@@ -0,0 +1,16 @@
+import { types } from "../types/types";
+
+
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+
+            break;
+
+        case types.logout:
+
+
+        default:
+            break;
+    }
+}
